bafc9b86112b315901aae16134d72d619ecde43a
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-07-12 13:38:50 +0900

    chore : compiler babel 적용

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents:
-      true |
-      {
-        ssr: true,
-        displayName: true,
-        pure: true,
-      },
-  },
 }
 
 module.exports = nextConfig
