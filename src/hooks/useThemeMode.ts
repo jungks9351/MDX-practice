e7ce5a689bba24a227100a49e2281b503aa5eaf6
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-07-21 18:24:01 +0900

    refactor : 테마 모드 훅으로 관리

---
+++ src/hooks/useThemeMode.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+const useThemeMode = () => {
+  const [themeMode, setThemeMode] = useState('')
+
+  const handleThemeMode = () => {
+    setThemeMode(themeMode === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme')
+
+    if (theme) setThemeMode(theme)
+  }, [])
+
+  useEffect(() => {
+    document.body.dataset.theme = themeMode
+    localStorage.setItem('theme', themeMode)
+  }, [themeMode])
+
+  return { themeMode, handleThemeMode }
+}
+
+export default useThemeMode
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,68 +1,34 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 
-import '../../public/static/font/index.css'
-import SunIcon from '../../public/static/images/sun_icon.svg'
-import MoonIcon from '../../public/static/images/moon_icon.svg'
+import '#static/font/index.css'
+import { darkTheme, lightTheme } from '@styles/theme'
+import GlobalStyle from '@styles/GlobalStyle'
 
-import Header from '../components/Header'
-import NavContainer from '../components/nav/NavContainer'
+import Header from '@components/Header'
+import Container from '@components/Container'
 
-import { darkTheme, lightTheme } from '../styles/theme'
-import GlobalStyle from '../styles/GlobalStyle'
+import useThemeMode from '@hooks/useThemeMode'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const [themeMode, setThemeMode] = useState('')
-
-  const handleThemeMode = () => {
-    setThemeMode(themeMode === 'light' ? 'dark' : 'light')
-  }
-
-  useEffect(() => {
-    if (localStorage.getItem('theme'))
-      setThemeMode(localStorage.getItem('theme'))
-  }, [])
-
-  useEffect(() => {
-    document.body.dataset.theme = themeMode
-    localStorage.setItem('theme', themeMode)
-  }, [themeMode])
+  const { themeMode, handleThemeMode } = useThemeMode()
 
   return (
     <ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"
-        />
         <meta name="description" content="My First Static Blog" />
         <meta name="keywords" content="nextjs,static,website" />
         <title>Blog</title>
       </Head>
       <GlobalStyle />
       <Container>
-        <Header>
-          <NavContainer />
-          <button onClick={handleThemeMode}>
-            {themeMode === 'light' ? <SunIcon /> : <MoonIcon />}
-          </button>
-        </Header>
+        <Header themeMode={themeMode} handleThemeMode={handleThemeMode} />
         <Component {...pageProps} />
       </Container>
     </ThemeProvider>
   )
 }
-
-const Container = styled.div`
-  width: calc(100%-2rem);
-  height: 100vh;
-  margin: 0 auto;
-  padding: 0 2rem;
-
-  background-color: ${({ theme }) => theme.bgColors.main};
-`
 
 export default MyApp
