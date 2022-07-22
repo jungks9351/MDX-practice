c9d827b37f4fa2d68a33a0cfe7f9371e6f1fc508
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-07-22 16:36:32 +0900

    style : 헤더 UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,24 +9,43 @@ import NavContainer from '@components/nav/NavContainer'
 const Header = ({ themeMode, handleThemeMode }) => {
   return (
     <HeaderWrapper>
-      <NavContainer />
-      <div>
-        <button onClick={handleThemeMode}>
-          {themeMode === 'light' ? <SunIcon /> : <MoonIcon />}
-        </button>
-      </div>
+      <DivideWrapper>
+        <HeaderLogo>JJ</HeaderLogo>
+        <div>
+          <button onClick={handleThemeMode}>
+            {themeMode === 'light' ? <StyledSunIcon /> : <StyledMoonIcon />}
+          </button>
+        </div>
+      </DivideWrapper>
+      <DivideWrapper>
+        <NavContainer />
+      </DivideWrapper>
     </HeaderWrapper>
   )
 }
 
 const HeaderWrapper = styled.header`
-  padding: 1rem 2rem;
+  padding: 0 2rem;
 
+  border-bottom: 1px solid #ecedf1;
+`
+const HeaderLogo = styled.h1`
+  font-size: 2.2rem;
+`
+
+const DivideWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding-top: 1.5rem;
+`
 
-  border-bottom: 1px solid #ecedf1;
+const StyledSunIcon = styled(SunIcon)`
+  font-size: 2.2rem;
+`
+
+const StyledMoonIcon = styled(MoonIcon)`
+  font-size: 1.8rem;
 `
 
 export default Header
